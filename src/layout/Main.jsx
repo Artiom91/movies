4277aab419b2a65c54b70da09860835e459382fa
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-04-13 10:52:36 +0300

    project complete

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Movies } from '../components/Movies'
 import { Preloader } from '../components/Preloader'
 import { Search } from '../components/Search'
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 class Main extends React.Component {
   state = {
     movies: [],
@@ -10,7 +12,7 @@ class Main extends React.Component {
   }
    
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e009805b&s=batman`)
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=batman`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }))
       .catch((err) => {
@@ -21,7 +23,7 @@ class Main extends React.Component {
   searchMovies = (str, type = 'all') => {
     this.setState({ loading: true })
     fetch(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=e009805b&s=${str}${
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }`
     )
